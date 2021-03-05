b9170bf99472c371d4d1854c0779f877d7b235c5
Zawatsuka <57181738+Zawatsuka@users.noreply.github.com>
2021-03-05 16:59:41 +0100

    maj

--- views/connect.php
+++ views/connect.php
@@ -6,35 +6,22 @@ ob_start();
     <!-- sondage -->
     <div class="row">
         <div class="col-12 col-sm-12 col-md-7 justify-content-center p-5">
-            <div class="d-flex justify-content-center">
-                <div class="flex-column">
-                    <div class="d-none d-sm-none d-md-block">
-                        <input type="text" size="70" class="p-3 m-4 connexion-form zoom"
-                            placeholder="Pseudo/Adresse Mail">
-                    </div>
-                    <div class="d-none d-sm-none d-md-block">
-                        <input type="password" size="70" class="p-3 m-4 connexion-form zoom" placeholder="Mot de passe">
-                    </div>
-                    <div class="d-block d-sm-block d-md-none">
-                        <input type="text" size="40" class="p-3 m-4 connexion-form zoom"
-                            placeholder="Pseudo/Adresse Mail">
-                    </div>
-                    <div class="d-block d-sm-block d-md-none">
-                        <input type="password" size="40" class="p-3 m-4 connexion-form zoom" placeholder="Mot de passe">
-                    </div>
-                    <div class="button-margin">
-                        <a href="#">
-                            <p class="color-link ml-3">T'as oublié ton mot de passe ?</p>
-                        </a>
-                    </div>
-                    <div class="button-margin zoom">
-                        <button class="px-5 py-4 m-3 button-connect"> Connexion </button>
-                    </div>
+        <h2>Connexion</h2>
+            <div class="input-group mb-3 mt-4">
+                <div class="input-group-prepend">
+                    <span class="input-group-text" id="basic-addon1">@</span>
                 </div>
-
+                <input type="text" class="form-control" placeholder="Adresse Email ＼(^ω^＼)" aria-label="Adresse Email ＼(^ω^＼)"
+                    aria-describedby="basic-addon1">
             </div>
+            <div class="input-group mb-3">
+                <input type="password" class="form-control" placeholder="Mot de Passe ᕙ༼*◕_◕*༽ᕤ" aria-label="Mot de Passe ᕙ༼*◕_◕*༽ᕤ"
+                    aria-describedby="basic-addon1">
+            </div>
+            <button type="submit" class="btn btn-outline-secondary mt-3">Connexion </button>
         </div>
     </div>
+</div>
 </div>
 <?php
 $mainContent = ob_get_clean();
--- views/userPage.php
+++ views/userPage.php
@@ -3,29 +3,33 @@ $typeRight= true;
 ob_start();
 ?>
 <a class="ml-3" href="/controllers/listOfUserCTRL.php">
-        <img src="/assets/img/fleche2.png" class="img-fluid mt-3 zoom" width="40" alt="fleche de retour">
-    </a>
-<div class="container text-center flipInX">
+    <img src="/assets/img/fleche2.png" class="img-fluid mt-3 zoom" width="40" alt="fleche de retour">
+</a>
+
+
+<div class="container text-center flipInX m-3 mt-5">
     <div class="row">
-        <div class="card mb-3 p-5 ml-5 mt-3" style="max-width: 540px;">
+        <div class="card mb-3" style="max-width: 540px;">
             <div class="row no-gutters">
                 <div class="col-md-4">
-                    <img src="/assets/img/user2.png" class="img-fluid" width="120" alt="image de profil utilisateur">
+                    <img src="/assets/img/user2.png" class="m-3 ml-4  img-fluid" width="200" alt="user">
                 </div>
                 <div class="col-md-8">
                     <div class="card-body p-5">
                         <h5 class="card-title"><?=$viewUser->pseudo;?></h5>
-                        <p class="card-text"><?php
-                        if($viewUser->admin ==1){
-                            echo '<a class="text-secondary" href="/controllers/listOfUserCTRL.php">Administration du site</a>'; 
-                             }       
-                            ?></p>
-                        <a class="text-secondary" href="/controllers/updateUserCTRL.php?idUser=<?=$viewUser->id;?>">
-                            <p class="card-text">
-                                modifier <img src="/assets/img/update_1.png" class="img-fluid" width="20"
-                                    alt="modifier">
-                            </p>
-                        </a>
+                        <p class="card-text">
+                            <?php
+            if($viewUser->admin ==1){
+                echo '<a class="text-secondary" href="/controllers/listOfUserCTRL.php">Administration du site</a>'; 
+            }?>
+                        </p>
+                        <p class="card-text">
+                            <a class="text-secondary" href="/controllers/updateUserCTRL.php?idUser=<?=$viewUser->id;?>">
+                                Modifier
+                                <img src="/assets/img/update_1.png" class="img-fluid" width="20"
+                                    alt="pictogramme modifier">
+                            </a>
+                        </p>
                         <a class="btn btn-secondary mt-3" data-toggle="collapse" href="#collapseExample" role="button"
                             aria-expanded="false" aria-controls="collapseExample">
                             Vos Informations <img src="/assets/img/plus_1.png" class="img-fluid" width="15" alt="plus">
@@ -34,8 +38,8 @@ ob_start();
                 </div>
             </div>
         </div>
-
     </div>
+
 </div>
 <div class="collapse" id="collapseExample">
     <div class="card card-body">
