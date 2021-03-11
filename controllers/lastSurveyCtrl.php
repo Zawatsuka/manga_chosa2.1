438fab05053ce50959e731e9657a778f5c6609f8
Zawatsuka <57181738+Zawatsuka@users.noreply.github.com>
2021-03-11 11:57:14 +0100

    maj

--- controllers/lastSurveyCtrl.php
+++ controllers/lastSurveyCtrl.php
@@ -3,6 +3,8 @@
     require_once(dirname(__FILE__).'/../models/TypeOfManga.php');
     $list_type = new typeOfManga();
     $getListType = $list_type->listOfType();
+    $idDeactivated = intval(trim(filter_input(INPUT_GET, 'idDeactivated', FILTER_SANITIZE_NUMBER_INT)));
+    var_dump($idDeactivated);
     include(dirname(__FILE__).'/../template/header.php');
     include(dirname(__FILE__).'/../views/lastSurvey.php');
     include(dirname(__FILE__).'/../template/footer.php');
